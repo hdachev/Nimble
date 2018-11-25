c01eb1d090306b9524c98257c139931b3c354bb9
Diharaw <shredhead94@gmail.com>
2018-11-25 23:38:18 +0530

    Geometry and Fullscreen render node.

--- src/render_node.cpp
+++ src/render_node.cpp
@@ -8,7 +8,7 @@ namespace nimble
 
 	// -----------------------------------------------------------------------------------------------------------------------------------
 
-	RenderNode::RenderNode(RenderGraph* graph) : m_enabled(true), m_graph(graph), m_id(g_last_node_id++), m_total_time_cpu(0.0f), m_total_time_gpu(0.0f)
+	RenderNode::RenderNode(RenderNodeType type, RenderGraph* graph) : m_enabled(true), m_render_node_type(type), m_graph(graph), m_id(g_last_node_id++), m_total_time_cpu(0.0f), m_total_time_gpu(0.0f)
 	{
 		m_passthrough_name = name();
 		m_passthrough_name += "_Passthrough";
@@ -19,66 +19,6 @@ namespace nimble
 	RenderNode::~RenderNode()
 	{
 
-	}
-
-	// -----------------------------------------------------------------------------------------------------------------------------------
-
-	void RenderNode::execute()
-	{
-		float cpu_time, gpu_time;
-
-		m_total_time_cpu = 0;
-		m_total_time_gpu = 0;
-
-		if (is_enabled())
-		{
-			
-			for (uint32_t i = 0; i < m_sub_passes.size(); i++)
-			{
-				const auto& pass = m_sub_passes[i];
-
-				Profiler::result(pass.first, cpu_time, gpu_time);
-
-				m_total_time_cpu += cpu_time;
-				m_total_time_gpu += gpu_time;
-
-				m_sub_pass_timings[i].first = cpu_time;
-				m_sub_pass_timings[i].second = gpu_time;
-
-				Profiler::begin_sample(pass.first);
-
-				// Execute subpass.
-				pass.second();
-
-				Profiler::end_sample(pass.first);
-			}
-		}
-		else
-		{
-			Profiler::result(m_passthrough_name, cpu_time, gpu_time);
-
-			m_total_time_cpu += cpu_time;
-			m_total_time_gpu += gpu_time;
-
-			Profiler::begin_sample(m_passthrough_name);
-
-			// Execute passthrough.
-			passthrough();
-
-			Profiler::end_sample(m_passthrough_name);
-		}
-	}
-
-	// -----------------------------------------------------------------------------------------------------------------------------------
-
-	void RenderNode::attach_sub_pass(const std::string& node_name, std::function<void(void)> function)
-	{
-		std::string formatted_name = name();
-		formatted_name += "_";
-		formatted_name += node_name;
-
-		m_sub_passes.push_back({ formatted_name, function });
-		m_sub_pass_timings.push_back({ 0.0f, 0.0f });
 	}
 
 	// -----------------------------------------------------------------------------------------------------------------------------------
@@ -136,17 +76,6 @@ namespace nimble
 
 	// -----------------------------------------------------------------------------------------------------------------------------------
 
-	void RenderNode::timing_sub_pass(const uint32_t& index, float& cpu_time, float& gpu_time)
-	{
-		if (index < sub_pass_count())
-		{
-			cpu_time = m_sub_pass_timings[index].first;
-			gpu_time = m_sub_pass_timings[index].second;
-		}
-	}
-
-	// -----------------------------------------------------------------------------------------------------------------------------------
-
 	void RenderNode::passthrough()
 	{
 
@@ -170,6 +99,112 @@ namespace nimble
 		m_render_targets[name] = rt.get();
 
 		return rt;
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	GeometryRenderNode::GeometryRenderNode(RenderGraph* graph) : RenderNode(RENDER_NODE_GEOMETRY, graph)
+	{
+
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	GeometryRenderNode::~GeometryRenderNode()
+	{
+
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	void GeometryRenderNode::execute()
+	{
+
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	FullscreenRenderNode::FullscreenRenderNode(RenderGraph* graph) : RenderNode(RENDER_NODE_FULLSCREEN, graph)
+	{
+
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	FullscreenRenderNode::~FullscreenRenderNode()
+	{
+
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	void FullscreenRenderNode::execute()
+	{
+		float cpu_time, gpu_time;
+
+		m_total_time_cpu = 0;
+		m_total_time_gpu = 0;
+
+		if (is_enabled())
+		{
+
+			for (uint32_t i = 0; i < m_sub_passes.size(); i++)
+			{
+				const auto& pass = m_sub_passes[i];
+
+				Profiler::result(pass.first, cpu_time, gpu_time);
+
+				m_total_time_cpu += cpu_time;
+				m_total_time_gpu += gpu_time;
+
+				m_sub_pass_timings[i].first = cpu_time;
+				m_sub_pass_timings[i].second = gpu_time;
+
+				Profiler::begin_sample(pass.first);
+
+				// Execute subpass.
+				pass.second();
+
+				Profiler::end_sample(pass.first);
+			}
+		}
+		else
+		{
+			Profiler::result(m_passthrough_name, cpu_time, gpu_time);
+
+			m_total_time_cpu += cpu_time;
+			m_total_time_gpu += gpu_time;
+
+			Profiler::begin_sample(m_passthrough_name);
+
+			// Execute passthrough.
+			passthrough();
+
+			Profiler::end_sample(m_passthrough_name);
+		}
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	void FullscreenRenderNode::attach_sub_pass(const std::string& node_name, std::function<void(void)> function)
+	{
+		std::string formatted_name = name();
+		formatted_name += "_";
+		formatted_name += node_name;
+
+		m_sub_passes.push_back({ formatted_name, function });
+		m_sub_pass_timings.push_back({ 0.0f, 0.0f });
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	void FullscreenRenderNode::timing_sub_pass(const uint32_t& index, float& cpu_time, float& gpu_time)
+	{
+		if (index < sub_pass_count())
+		{
+			cpu_time = m_sub_pass_timings[index].first;
+			gpu_time = m_sub_pass_timings[index].second;
+		}
 	}
 
 	// -----------------------------------------------------------------------------------------------------------------------------------
--- src/render_node.h
+++ src/render_node.h
@@ -9,26 +9,30 @@ namespace nimble
 {
 	class RenderGraph;
 
+	enum RenderNodeType
+	{
+		RENDER_NODE_GEOMETRY = 0,
+		RENDER_NODE_FULLSCREEN = 1,
+		RENDER_NODE_COMPUTE = 2
+	};
+
 	class RenderNode
 	{
 	public:
-		RenderNode(RenderGraph* graph);
+		RenderNode(RenderNodeType type, RenderGraph* graph);
 		~RenderNode();
 
-		void execute();
-		void attach_sub_pass(const std::string& node_name, std::function<void(void)> function);
 		RenderTarget* render_target_by_name(const std::string& name);
 		RenderTarget* render_target_dependecy_by_name(const std::string& name);
 		Buffer* buffer_dependecy_by_name(const std::string& name);
 		void set_dependency(const std::string& name, RenderTarget* rt);
 		void set_dependency(const std::string& name, Buffer* buffer);
 		void timing_total(float& cpu_time, float& gpu_time);
-		void timing_sub_pass(const uint32_t& index, float& cpu_time, float& gpu_time);
 
 		// Inline getters
+		inline RenderNodeType type() { return m_render_node_type; }
 		inline bool is_enabled() { return m_enabled; }
 		inline uint32_t id() { return m_id; }
-		inline uint32_t sub_pass_count() { return m_sub_passes.size(); }
 
 		// Inline setters
 		inline void enable() { m_enabled = true; }
@@ -36,6 +40,7 @@ namespace nimble
 
 		// Virtual methods
 		virtual void passthrough();
+		virtual void execute() = 0;
 		virtual bool initialize() = 0;
 		virtual void shutdown() = 0;
 		virtual std::string name() = 0;
@@ -46,17 +51,43 @@ namespace nimble
 
 	protected:
 		RenderGraph* m_graph;
+		float m_total_time_cpu;
+		float m_total_time_gpu;
+		std::string m_passthrough_name;
 		
 	private:
 		bool m_enabled;
 		uint32_t m_id;
+		RenderNodeType m_render_node_type;
 		std::unordered_map<std::string, RenderTarget*> m_render_targets;
 		std::unordered_map<std::string, RenderTarget*> m_rt_dependecies;
 		std::unordered_map<std::string, Buffer*> m_buffer_dependecies;
+	};
+
+	class GeometryRenderNode : public RenderNode
+	{
+	public:
+		GeometryRenderNode(RenderGraph* graph);
+		~GeometryRenderNode();
+
+		void execute() override;
+	};
+
+	class FullscreenRenderNode : public RenderNode
+	{
+	public:
+		FullscreenRenderNode(RenderGraph* graph);
+		~FullscreenRenderNode();
+
+		void execute() override;
+		void attach_sub_pass(const std::string& node_name, std::function<void(void)> function);
+		void timing_sub_pass(const uint32_t& index, float& cpu_time, float& gpu_time);
+
+		// Inline getters
+		inline uint32_t sub_pass_count() { return m_sub_passes.size(); }
+
+	private:
 		std::vector<std::pair<std::string, std::function<void(void)>>> m_sub_passes;
 		std::vector<std::pair<float, float>> m_sub_pass_timings;
-		float m_total_time_cpu;
-		float m_total_time_gpu;
-		std::string m_passthrough_name;
 	};
 }
