a7bbcd4b4e0f0ae7a34e3a116c15e07e69f8cc6b
Diharaw <shredhead94@gmail.com>
2018-11-29 22:38:31 +0530

    ShaderCache implementation.

--- src/shader_cache.cpp
+++ src/shader_cache.cpp
@@ -1,6 +1,29 @@
 #include "shader_cache.h"
+#include "shader_library.h"
 
 namespace nimble
 {
+	std::unordered_map<std::string, std::weak_ptr<ShaderLibrary>> ShaderCache::m_library_cache;
 
+	// -----------------------------------------------------------------------------------------------------------------------------------
+
+	std::shared_ptr<ShaderLibrary> ShaderCache::load_library(const std::string& vs, const std::string& fs)
+	{
+		std::string id = "vs:";
+		id += vs;
+		id += "-fs:";
+		id += fs;
+
+		if (m_library_cache.find(id) != m_library_cache.end() && !m_library_cache[id].expired())
+			return m_library_cache[id].lock();
+		else
+		{
+			auto library = std::make_shared<ShaderLibrary>(vs, fs);
+			m_library_cache[id] = library;
+
+			return library;
+		}
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------------------------
 }
--- src/shader_cache.h
+++ src/shader_cache.h
@@ -1,11 +1,19 @@
 #pragma once
 
-#include "static_hash_map.h"
+#include <unordered_map>
+#include <memory>
+#include <string>
 
 namespace nimble
 {
+	class ShaderLibrary;
+
 	class ShaderCache
 	{
+	public:
+		static std::shared_ptr<ShaderLibrary> load_library(const std::string& vs, const std::string& fs);
 
+	private:
+		static std::unordered_map<std::string, std::weak_ptr<ShaderLibrary>> m_library_cache;
 	};
 }
