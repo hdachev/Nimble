e1ad91924e73465a8872e23e652f04d526cac7bb
Diharaw <shredhead94@gmail.com>
2019-04-16 23:25:17 +0530

    SSR shader progress.

--- src/shader/post_process/ssr/ssr_cs.glsl
+++ src/shader/post_process/ssr/ssr_cs.glsl
@@ -17,18 +17,15 @@ layout (local_size_x = SSR_NUM_THREADS, local_size_y = SSR_NUM_THREADS) in;
 // CONSTANTS --------------------------------------------------------
 // ------------------------------------------------------------------
 
-const float kRayStep = 0.1;
-const float kMinRayStep = 0.1;
-const int kMaxSteps = 30;
-const float kSearchDist = 5.0;
-const int kNumBinarySearchSteps = 5;
-const float kReflectionSpecularFalloffExponent = 3.0;
+const float RAY_STEP_SIZE = 0.1;
+const int 	MAX_RAY_MARCH_SAMPLES = 32;
+const int 	MAX_BINARY_SEARCH_SAMPLES = 5;
 
 // ------------------------------------------------------------------
 // UNIFORMS ---------------------------------------------------------
 // ------------------------------------------------------------------
 
-layout (binding = 0, r32f) uniform image2D i_SSR;
+layout (binding = 0, rgba16f) uniform image2D i_SSR;
 
 uniform sampler2D s_HiZDepth;
 uniform sampler2D s_Metallic;
@@ -40,50 +37,44 @@ uniform float u_HiZLevels;
 // FUNCTIONS --------------------------------------------------------
 // ------------------------------------------------------------------
 
-vec3 binary_search(inout vec3 dir, inout vec3 hit_coord, inout float out_depth)
+vec3 binary_search(in vec3 pos, in vec3 prev_pos)
 {
-	float depth;
-	vec4 projected_coord;
+	vec3 min_sample = prev_pos;
+	vec3 max_sample = pos;
+	vec3 mid_sample;
 
-	for (int i = 0; i < kNumBinarySearchSteps; i++)
+	for (int i = 0; i < MAX_BINARY_SEARCH_SAMPLES; i++)
 	{
-		projected_coord = projMat * vec4(hit_coord, 1.0);
-		projected_coord.xy /= projected_coord.w;
-		projected_coord.xy = projected_coord.xy * 0.5 + 0.5;
+		mid_sample = mix(min_sample, max_sample, 0.5);
+		float z_val = texture(s_Depth, mid_sample.xy).x;
 
-		depth = get_view_space_depth(projected_coord.xy, texture(s_Depth, projected_coord.xy).r);
-
-		out_depth = hit_coord.z - depth;
-
-		dir *= 0.5;
-
-		if (out_depth > 0.0)
-			hit_coord += dir;
+		if (mid_sample.z > z_val)
+			max_sample = mid_sample;
 		else
-			hit_coord -= dir;
+			min_sample = mid_sample;
 	}
 
-	projected_coord = projMat * vec4(hit_coord, 1.0);
-	projected_coord.xy /= projected_coord.w;
-	projected_coord.xy = projected_coord.xy * 0.5 + 0.5;
-
-	return vec3(projected_coord.xy, depth);
+	return mid_sample;
 }
 
 // ------------------------------------------------------------------
 
 vec3 ray_march(in vec3 dir, in vec3 pos)
 {
-	for (int ray_step_idx = 0; ray_step_idx < kMaxSteps; ray_step_idx++)
+	vec3 prev_ray_sample = pos;
+
+	for (int ray_step_idx = 0; ray_step_idx < MAX_RAY_MARCH_SAMPLES; ray_step_idx++)
 	{
-		vec3 ray_sample = (ray_step_idx * kRayStep) * dir + pos;
-		float z_val = texture(s_Depth, ray_sample.xy);
+		vec3 ray_sample = (ray_step_idx * RAY_STEP_SIZE) * dir + pos;
+		float z_val = texture(s_Depth, ray_sample.xy).x;
 
 		if (ray_sample.z > z_val)
-		{
-
-		}
+			return binary_search(ray_sample, prev_ray_sample);
+		
+		prev_ray_sample = ray_sample;
 	}
+
+	return vec3(0.0);
 }
 
 // ------------------------------------------------------------------
@@ -96,13 +87,13 @@ void main()
 	vec2 tex_coord = vec2(gl_GlobalInvocationID.x, gl_GlobalInvocationID.y) / vec2(size);
 
 	// Fetch depth from hardware depth buffer
-	float depth = textureLod(s_HiZDepth, tex_coord, 0);
+	float depth = textureLod(s_HiZDepth, tex_coord, 0).x;
 
 	// Reconstruct world space position
 	vec3 world_pos = world_position_from_depth(tex_coord, depth);
 
 	// Get world space normal from G-Buffer
-	vec3 normal = textureLod(s_Normal, tex_coord, 0);
+	vec3 normal = textureLod(s_Normal, tex_coord, 0).xyz;
 
 	vec4 screen_pos = vec4(tex_coord, depth, 1.0);
 
